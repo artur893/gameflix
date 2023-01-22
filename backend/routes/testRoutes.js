60967c4a0789719bc0ad4de2201d4875e99a456a
artur893 <arthal893@gmail.com>
2023-01-22 14:38:22 +0100

    Init, controllers, routes

---
+++ backend/routes/testRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/testController')
+
+router.route('/').get(getGoals).post(setGoals)
+router.route('/:id').put(updateGoals).delete(deleteGoals)
+
+module.exports = router
