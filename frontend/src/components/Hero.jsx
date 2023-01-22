8d10aa8bcc4617e3494f3cbf287956a1dd656e9f
artur893 <arthal893@gmail.com>
2023-01-23 00:06:36 +0100

    Login design

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,9 +2,9 @@ function Hero() {
     return (
         <div className="hero w-screen h-full text-font bg-[url('https://www.neuropeakpro.com/wp-content/uploads/2018/12/Blog-42.jpg')]">
             <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Unlimited games</h1>
-                    <p className="text-3xl py-6">Play anywhere. Get fun anytime</p>
+                <div className="max-w-36">
+                    <h1 className="text-6xl font-bold">Unlimited games</h1>
+                    <p className="text-3xl py-6">Play anywhere. Get fun anytime.</p>
                     <p className="pb-6">Ready to play? Click the button and register an account.</p>
                     <button className="btn btn-accent btn-wide text-font">Get Started</button>
                 </div>
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom'
+
 function Nav(props) {
     return (
         <header className="flex justify-between w-full absolute">
-            <h1 className="text-accent font-bold text-5xl p-8">GAMEFLIX</h1>
-            <button class="btn btn-accent m-8 text-font">LOGIN</button>
+            <Link to='/'><h1 className="text-accent font-bold text-5xl p-8">GAMEFLIX</h1></Link>
+            <Link to='/login'><button class="btn btn-accent m-8 text-font">LOGIN</button></Link>
         </header>
     )
 }
