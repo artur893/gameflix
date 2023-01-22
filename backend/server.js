bac294f8745c03721389f7b9326d5d507ecca383
artur893 <arthal893@gmail.com>
2023-01-22 16:29:54 +0100

    REST API

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
@@ -12,5 +16,5 @@ app.use('/api/games', require('./routes/testRoutes'))
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server running on port: ${port}`))
+app.listen(port, () => console.log(`Server running on port: ${port}`.green))
 
